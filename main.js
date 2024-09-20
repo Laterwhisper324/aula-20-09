e62841a3da7b0853b6a84367b28ef1a94ee48d7d
laterwhisper423 <167800572+Laterwhisper324@users.noreply.github.com>
2024-09-20 17:39:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+        function abrirmodal(){
+    document.getElementById('fundodomodal').style.display='block'
+}
+        function fechar(){
+    document.getElementById('fundodomodal').style.display='none'
+}
+document.getElementById('close').onclick = function(){
+    document.getElementById('fundodomodal').style.display='none'
+}
